be4f56ee21bd462d41315dbf8ad2a02c4a102950
keza-git-ange <kezaange231@gmail.com>
2025-02-18 12:00:35 +0200

    Add files via upload

---
+++ zdxcv tjyukh.jl
@@ -0,0 +1,65 @@
+[1mdiff --git a/Employee Salaries.txt b/Employee Salaries.txt[m
+[1mdeleted file mode 100644[m
+[1mindex 4e1be10..0000000[m
+[1m--- a/Employee Salaries.txt[m	
+[1m+++ /dev/null[m
+[36m@@ -1,16 +0,0 @@[m
+[31m-Adelev	 $106,280 [m
+[31m-Alexw	 $135,049 [m
+[31m-Diegos	 $217,238 [m
+[31m-Gradya	 $63,569 [m
+[31m-Henriettam	 $203,418 [m
+[31m-Isaiahl	 $179,026 [m
+[31m-Johannaljonis	 $145,127 [m
+[31m-Leeg	 $181,821 [m
+[31m-Lidiah	 $123,694 [m
+[31m-Lynner	 $93,934 [m
+[31m-Meganb	 $99,726 [m
+[31m-Miriamg	 $192,831 [m
+[31m-Nestorw	 $57,327 [m
+[31m-Pattif	 $4,511,840 [m
+[31m-Pradeepg	 $94,884 [m
+[31m-Kevstrat	$35,015[m
+\ No newline at end of file[m
+[1mdiff --git a/KCC Logo.png b/KCC Logo.png[m
+[1mdeleted file mode 100644[m
+[1mindex 14f6de5..0000000[m
+Binary files a/KCC Logo.png and /dev/null differ
+[1mdiff --git a/secret recipe.htm b/secret recipe.htm[m
+[1mdeleted file mode 100644[m
+[1mindex a1428c5..0000000[m
+[1m--- a/secret recipe.htm[m	
+[1m+++ /dev/null[m
+[36m@@ -1,31 +0,0 @@[m
+[31m-<!DOCTYPE html>[m
+[31m-<html>[m
+[31m-<body>[m
+[31m-[m
+[31m-<h1>Ingredients</h1>[m
+[31m-<ul>[m
+[31m-<li>1 cup Butter or margarine, softened</li>[m
+[31m-<li>3/4 cup Sugar</li>[m
+[31m-<li>3/4 cup Brown sugar, packed</li>[m
+[31m-<li>2 Eggs, large</li>[m
+[31m-<li>2 teaspoons Vanilla</li>[m
+[31m-<li>2 1/4 cups Flour, unsifted</li>[m
+[31m-<li>1 teaspoon Baking soda</li>[m
+[31m-<li>1/2 teaspoon Salt</li>[m
+[31m-<li>1 cup Walnuts or pecans, chopped (optional)</li>[m
+[31m-<li>2 cups KCC Bittersweet 60% Cacao Baking Chips</li>[m
+[31m-</ul>[m
+[31m-<h1>Baking instructions</h1>[m
+[31m-<ul>[m
+[31m-<li>Heat oven to 500ºF.</li>[m
+[31m-<li>Stir flour with baking soda and salt; set aside.</li>[m
+[31m-<li>In large mixing bowl, beat butter with sugar, and brown sugar at medium speed until creamy and lightened in color.</li>[m
+[31m-<li>Add eggs and vanilla, one at a time. Mix on low speed until incorporated.</li>[m
+[31m-<li>Gradually blend dry mixture into creamed mixture. Stir in nuts and chocolate chips.</li>[m
+[31m-<li>Drop by tablespoon onto ungreased cookie sheets.</li>[m
+[31m-<li>Bake for 9 to 11 minutes or until chocolate chip cookies are golden brown.</li>[m
+[31m-<li>Enjoy!</li>[m
+[31m-</ul>[m
+[31m-[m
+[31m-</body>[m
+[31m-</html>[m
+\ No newline at end of file[m
